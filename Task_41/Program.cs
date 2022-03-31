23800a70f982f269388f005996586dc6c6b8679c
Терехин Максим Александрович <MTerekhin@synergy.ru>
2022-03-31 20:32:12 +0300

    Finish code

---
+++ Task_41/Program.cs
@@ -0,0 +1,33 @@
+﻿int i = 0;
+bool a = true;
+string word = "";
+while (a = true)
+{
+    Console.Write("Введите число, если хотетите прекратить введите слово 'end': ");
+    word = Console.ReadLine();
+    if (word == "end")
+    {
+        if (i == 0)
+        {
+            Console.Write("Вы не ввели ни одного числа :(");
+        }
+        break;
+    }
+    if (Convert.ToInt32(word) > 0)
+    {
+        i++;
+    }
+    Console.WriteLine();
+}
+if(i == 1)
+{
+    Console.Write($"Вы ввели только {i} число больше нуля");
+}
+if(i > 1 && i < 5)
+{
+    Console.Write($"Вы ввели только {i} числа больше нуля");
+}
+if(i > 4)
+{
+    Console.Write($"Вы ввели только {i} чисел больше нуля");
+}
